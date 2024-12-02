d451900c3706bd4d0f3394a68b437341210dee92
branden kennedy <brandenk88@gmail.com>
2024-12-01 20:48:34 -0800

    changed API key and hid it...

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build.gradle.kts (usually in the root project folder)
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin) apply false
 }
+
+val googleApiKey: String? = rootProject.findProperty("google_api_key") as String?
