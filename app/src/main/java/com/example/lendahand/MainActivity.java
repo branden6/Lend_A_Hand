234b82bcebc830be305b08a907e211f28c4aa8e6
branden kennedy <brandenk88@gmail.com>
2024-12-01 20:05:19 -0800

    added in more activities.... having trouble with the custom location functionality. Bit more bugs to hash out and some more functionality for the results page, almost done...

--- app/src/main/java/com/example/lendahand/MainActivity.java
+++ app/src/main/java/com/example/lendahand/MainActivity.java
@@ -46,14 +46,6 @@ public class MainActivity extends AppCompatActivity { //homescreen
         });
 
 
-
-
-
-
-
-
-
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/lendahand/SearchFilterActivity.java
+++ app/src/main/java/com/example/lendahand/SearchFilterActivity.java
@@ -1,9 +1,14 @@
 package com.example.lendahand;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +18,11 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SearchFilterActivity extends AppCompatActivity {
     EditText editTextMinPrice, editTextMaxPrice;
+    ImageView imageViewBack;
+    RadioGroup locationGroup;
+    RadioButton radioButtonCurrentLocation, radioButtonCustomLocation;
+
+    Button seeResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +30,10 @@ public class SearchFilterActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_search_filter);
 
-        //addDollarSignToPrices(); //Just adding a dollar sign on the entered values for min/max price
+        //addDollarSignToPrices(); //doesnt work, Just adding a dollar sign on the entered values for min/max price
+
+        SelectedRadioLocations(); //if custom radius is selected, starts a new Google Maps view activity
+        filterBackButton(); //if the back button is clicked while in the filters, it goes back to the homescreen
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -28,7 +41,27 @@ public class SearchFilterActivity extends AppCompatActivity {
             return insets;
         });
     }
+    public void filterBackButton(){
+        imageViewBack = findViewById(R.id.imageViewBack);
+        imageViewBack.setOnClickListener(v -> { //if the back button is clicked while in the filters, it goes back to the homescreen
+            Intent intent = new Intent(SearchFilterActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+    }
 
+    public void SelectedRadioLocations(){
+        radioButtonCustomLocation = findViewById(R.id.radioButtonCustomLocation);
+        locationGroup = findViewById(R.id.radioGroupLocation); //getting radioGroup for location
+        locationGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() { //if custom radius is selected, starts a new Google Maps view activity
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.radioButtonCustomLocation) {
+                    Intent intent = new Intent(SearchFilterActivity.this, CustomLocationActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
     public void addDollarSignToPrices(){
         editTextMinPrice = findViewById(R.id.editTextMinPrice);
         editTextMaxPrice = findViewById(R.id.editTextMaxPrice);
