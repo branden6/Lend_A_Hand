968f3498be8803a905360c82873982fa2c717a1e
Veer Ramrakhiani <veer.ramrakhiani@gmail.com>
2024-12-03 01:15:46 -0800

    added task 4

---
+++ app/src/main/java/com/example/lendahand/PaymentConfirmation.java
@@ -0,0 +1,31 @@
+package com.example.lendahand;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PaymentConfirmation extends AppCompatActivity {
+    Button returnHome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_payment_confirmation);
+
+        returnHome = findViewById(R.id.returnHome);
+        returnHome.setOnClickListener(v -> {
+            Intent intent = new Intent(PaymentConfirmation.this, ListingPage.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
+        });
+
+    }
+}
