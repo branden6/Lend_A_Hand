56ad06107f2dfcc5bc1140dde42090aa0023f030
branden kennedy <brandenk88@gmail.com>
2024-12-02 18:10:17 -0800

    Trying to get the map to work.

--- app/src/main/java/com/example/lendahand/CustomLocationActivity.java
+++ app/src/main/java/com/example/lendahand/CustomLocationActivity.java
@@ -1,27 +1,17 @@
 package com.example.lendahand;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.FragmentActivity;
 
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
+
 import android.os.Bundle;
-import android.widget.ImageView;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.example.lendahand.databinding.ActivityCustomLocationBinding;
-import com.google.android.gms.tasks.OnSuccessListener;
-import android.widget.Button;
+
 
 public class CustomLocationActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -37,6 +27,7 @@ public class CustomLocationActivity extends AppCompatActivity implements OnMapRe
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        assert mapFragment != null;
         mapFragment.getMapAsync(this);
     }
 
