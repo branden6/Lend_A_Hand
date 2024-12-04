23158d6b45aa51ea282f3c7cb8307f919bf670c6
kaanspgl <kaansapoglu@gmail.com>
2024-12-04 05:54:06 +0300

    Revert "changed API key and hid it..."
    This reverts commit d451900c3706bd4d0f3394a68b437341210dee92.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,5 @@
-// Top-level build.gradle.kts (usually in the root project folder)
-
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin) apply false
 }
-
-val googleApiKey: String? = rootProject.findProperty("google_api_key") as String?
